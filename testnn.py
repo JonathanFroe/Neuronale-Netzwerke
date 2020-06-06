bc5b5149db46c3976928a986ab0095aa313be0c6
JonathanFroe <57289773+JonathanFroe@users.noreply.github.com>
2020-06-06 19:21:49 +0200

    Neuronales Netwerk zum Erkennen von Gesichtern auf Bildern

--- testnn.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import filedialog
-root = tk.Tk()
-root.withdraw()
-rep = filedialog.askopenfilenames(parent=root,
-                                      initialdir='data/test/',
-                                      initialfile='',
-                                      filetypes=[("JPEG", "*.jpg")])
-print(rep)
